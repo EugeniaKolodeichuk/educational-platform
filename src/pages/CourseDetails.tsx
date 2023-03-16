c8eecde8966d43b74c5d554d8ab758273aac8513
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2023-03-16 20:13:47 +0200

    add saving video progress to local storage

--- src/pages/CourseDetails.tsx
+++ src/pages/CourseDetails.tsx
@@ -5,31 +5,70 @@ import { getCourseInfo } from '../service/app';
 import { CourseObject, Lesson } from '../types/Course';
 import { AiOutlineLock, AiOutlineUnlock } from 'react-icons/ai';
 
+interface VideoOptions {
+  courseTitle: string;
+  lessonOrder: number;
+  currentTime: number;
+}
+
 const CourseDetails = () => {
   const { courseId } = useParams();
   const [course, setCourse] = useState<CourseObject | undefined>();
-  const [videoLesson, setVideoLesson] = useState<number | null>(null);
+  const [lessonNumber, setLessonNumber] = useState<number | null>(null);
+  const [startAt, setStartAt] = useState<number>(0);
+  const [courseTitle, setCourseTitle] = useState('');
 
   useEffect(() => {
     try {
       if (courseId) {
         getCourseInfo(courseId).then(data => {
-          console.log('data', data);
           setCourse(data);
         });
       }
     } catch {
       console.error();
     }
-    console.log('id', courseId);
   }, [courseId]);
+
+  const savedVideoOptions = localStorage.getItem('videoOptions');
+
+  useEffect(() => {
+    if (savedVideoOptions) {
+      const videoOptions: VideoOptions = JSON.parse(savedVideoOptions);
+      setCourseTitle(videoOptions.courseTitle);
+      if (courseTitle === course?.title) {
+        setStartAt(videoOptions.currentTime);
+        setLessonNumber(videoOptions.lessonOrder);
+
+        onLessonVideoPlay(videoOptions.lessonOrder);
+      }
+    }
+  }, [course]);
+
+  const onLessonVideoPlay = (lessonOrder: number) => {
+    setLessonNumber(lessonOrder);
+  };
+
+  const onSaveVideoProgress = (courseTitle: string, lessonOrder: number, currentTime: number) => {
+    const videoOptions: VideoOptions = {
+      courseTitle,
+      lessonOrder,
+      currentTime,
+    };
+    localStorage.setItem('videoOptions', JSON.stringify(videoOptions));
+  };
+
+  const sortedLessons = course?.lessons
+    ?.map(lesson => {
+      return { ...lesson, order: lesson.order };
+    })
+    .sort((a: any, b: any) => a.order - b.order);
 
   return (
     <div>
       {course ? (
         <div>
           <h3>{course.title}</h3>
-
           <ReactHlsPlayer
             src={course.meta.courseVideoPreview.link}
             poster={`${course.previewImageLink}/cover.webp`}
@@ -38,31 +77,45 @@ const CourseDetails = () => {
             width="300px"
             height="auto"
           />
-
           <p>{course.description}</p>
           <p>
             This course contains {course.lessons.length}{' '}
             {course.lessons.length > 1 ? 'lessons' : 'lesson'}:
           </p>
-          {course.lessons.length &&
-            course.lessons.map((lesson: Lesson) => (
+          {sortedLessons?.length &&
+            sortedLessons.map((lesson: Lesson) => (
               <div key={lesson.id}>
-                <p onClick={() => setVideoLesson(lesson.order)}>
+                <p
+                  onClick={() => onLessonVideoPlay(lesson.order)}
+                  className={
+                    lesson.order === lessonNumber && lesson.status === 'unlocked' ? 'font-bold' : ''
+                  }
+                >
                   {lesson.order}. {lesson.title}
                   <span>
                     {lesson.status === 'locked' ? <AiOutlineLock /> : <AiOutlineUnlock />}
                   </span>
                 </p>
-                {videoLesson === lesson.order && lesson.status === 'unlocked' && (
+                {lesson.order === lessonNumber && lesson.status === 'unlocked' && (
                   <ReactHlsPlayer
                     src={lesson.link}
-                    autoPlay={true}
+                    autoPlay={false}
                     controls={true}
                     width="300px"
                     height="auto"
+                    onPause={e =>
+                      onSaveVideoProgress(course.title, lesson.order, e.currentTarget.currentTime)
+                    }
+                    hlsConfig={{
+                      startPosition:
+                        savedVideoOptions &&
+                        JSON.parse(savedVideoOptions).lessonOrder === lesson.order
+                          ? startAt
+                          : -1,
+                    }}
                   />
                 )}
-                {videoLesson === lesson.order && lesson.status === 'locked' && (
+                {lesson.order === lessonNumber && lesson.status === 'locked' && (
                   <p>This lesson locked</p>
                 )}
               </div>
