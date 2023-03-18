583527796d2a9a64cb0108b491f8cecb7f0d67bb
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2023-03-18 23:56:55 +0200

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <section className="bg-[#ffffff8c] rounded-lg mt-5">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-gray-900">
+            404
+          </h1>
+          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+            Something's missing.
+          </p>
+          <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+            Sorry, we can't find that page. You'll find lots to explore on the home page.{' '}
+          </p>
+          <a
+            href="/"
+            className="inline-flex border border-[#FD8469] text-[#FD8469] bg-primary-600 hover:bg-[#FD8469] hover:text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4"
+          >
+            Back to Homepage
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default NotFound;
